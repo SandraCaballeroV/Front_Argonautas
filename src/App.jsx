afa2bd69d391addc2033b661877c8cd46dc3e216
unknown <sandracaballero11@gmail.com>
2021-12-07 11:56:44 -0500

    modificacion de rutas en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
+import { Auth0Provider } from '@auth0/auth0-react';
 import PrivateLayout from "layouts/PrivateLayout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthContext } from "context/authContext";
 import { UserContext } from "context/userContext";
 import Index from "pages/Index";
 import Usuarios from "pages/usuarios/index";
-import IndexCategory1 from "pages/category1/Index";
-import Category1 from "pages/category1/CategoryPage1";
+import IndexCategory1 from 'pages/category1/Index';
+import Category1 from 'pages/category1/CategoryPage1';
 import { ApolloProvider, createHttpLink, ApolloClient, InMemoryCache } from '@apollo/client';
 import EditarUsuario from "pages/usuarios/editar";
 import "styles/globals.css";
@@ -17,67 +18,30 @@ import Login from "pages/auth/login";
 import { setContext } from '@apollo/client/link/context';
 
 
-// import PrivateRoute from 'components/PrivateRoute';
-
-const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/graphql',
-});
-
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = JSON.parse(localStorage.getItem('token'));
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: authLink.concat(httpLink),
-});
 
 function App() {
   const [userData, setUserData] = useState({});
-  const [authToken, setAuthToken] = useState("");
-  const [loadingAuth, setLoadingAuth] = useState(true);
-
-  const setToken = (data) => {
-    setAuthToken(data);
-    console.log("dt token", data);
-    if (data) {
-      localStorage.setItem("token", JSON.stringify(data));
-    } 
-  };
 
   return (
-    <ApolloProvider client={client}>
-      <AuthContext.Provider value={{ authToken, setToken, loadingAuth }}>
-        <UserContext.Provider value={{ userData, setUserData }}>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<PrivateLayout />}>
-                <Route path="" element={<Index />} />
-                <Route path="usuarios/" element={<Usuarios />} />
-                <Route
-                  path="usuarios/editar/:_id"
-                  element={<EditarUsuario />}
-                />
-                <Route path="category1" element={<IndexCategory1 />} />
-                <Route path="category1/page1" element={<Category1 />} />
-              </Route>
-              <Route path="/auth" element={<AuthLayout />}>
-                <Route path="register" element={<Register />} />
-                <Route path="login" element={<Login />} />
-              </Route>
-            </Routes>
-          </BrowserRouter>
-        </UserContext.Provider>
-      </AuthContext.Provider>
-    </ApolloProvider>
+    <Auth0Provider
+      domain='misiontic-concesionario.us.auth0.com'
+      clientId='WsdhjjQzDLIZEHA6ouuxXGxFONFGAQ4g'
+      redirectUri='http://localhost:3000/admin'
+      audience='api-autenticacion-concesionario-mintic'
+    >
+      <UserContext.Provider value={{ userData, setUserData }}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<PrivateLayout />}>
+              <Route path='' element={<Index />} />
+              <Route path='usuarios' element={<Usuarios />} />
+              <Route path='category1' element={<IndexCategory1 />} />
+              <Route path='category1/page1' element={<Category1 />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </UserContext.Provider>
+    </Auth0Provider>
   );
 }
 
